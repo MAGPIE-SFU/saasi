f948fbdfcd5128b2e52c951d27392d8f78f70c42
Ivan S Gill <ivan_gill@sfu.ca>
2025-04-01 17:37:25 -0700

    Linter

--- R/lambda_mu_generator.R
+++ R/lambda_mu_generator.R
@@ -1,14 +1,14 @@
 # c1
-c1 <- function(lambda, mu, psi){
+c1 <- function(lambda, mu, psi) {
   return(abs(sqrt((lambda - mu - psi)^2 + (4 * lambda * psi))))
 }
 
 # c2
-c2 <- function(lambda, mu, psi){
+c2 <- function(lambda, mu, psi) {
   return(-(lambda - mu - psi) / (c1(lambda, mu, psi)))
 }
 
-q <- function(lambda, mu, psi, t){
+q <- function(lambda, mu, psi, t) {
   d1 <- 2 * (1 - (c2(lambda, mu, psi))^2)
   d2 <- exp(-c1(lambda, mu, psi) * t) * (1 - c2(lambda, mu, psi))^2
   d3 <- exp(c1(lambda, mu, psi) * t) * (1 + c2(lambda, mu, psi))^2
@@ -20,7 +20,7 @@ probability_density <- function(lambda, mu, psi,
   # Add checks for invalid inputs
   sorted_x <- sort(internal_node_times)
   sorted_y <- sort(leaf_node_times)
-  d1 <- length(internal_node_times) *log(lambda)
+  d1 <- length(internal_node_times) * log(lambda)
   d2 <- sum(-log(q(lambda, mu, psi, sorted_x)))
   d3 <- sum(log(psi) + log(q(lambda, mu, psi, sorted_y)))
   ret <- d1 + d2 + d3
@@ -37,21 +37,20 @@ sim_mle_lm <- function(phy, lambda = 2, mu = 0.5, psi = 0.5,
   nnode <- phy[["Nnode"]] * 2 + 1
   nleaf_node <- nnode - phy[["Nnode"]]
   leaf_node_times <- node_times[1:nleaf_node]
-  internal_node_times <- node_times[(nleaf_node+1):nnode]
+  internal_node_times <- node_times[(nleaf_node + 1):nnode]
 
-  # Returns negative log likelihood
-  nLL <- function(lambda, mu) {
+  negative_log_likelihood <- function(lambda, mu) {
     positive_ret <- probability_density(lambda, mu, psi,
                                         internal_node_times, leaf_node_times)
     return(-positive_ret)
   }
 
-  fit <- stats4::mle(nLL,
+  fit <- stats4::mle(negative_log_likelihood,
                      start = list(lambda = lambda, mu = mu),
                      method = method,
                      lower = c(0.1, 0.05),
                      upper = c(10, 10))
-  
+
   ret <- unname(c(stats4::coef(fit)[1], stats4::coef(fit)[2]))
   return(ret)
 }
