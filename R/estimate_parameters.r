f43acfa081fa46283cea9c0c1ae9e8a789e8f6db
rennydoig <rennyd@sfu.ca>
2025-09-26 18:53:44 -0700

    Adding log-sum-exponential function and fixing root loglikelihood evaluation

--- R/estimate_parameters.r
+++ R/estimate_parameters.r
@@ -15,11 +15,16 @@ estimate_parameters <- function(phylobject, par_df, n_state, n_node, post_order_
         Q <- matrix(x[1:n_state^2+2*n_state], nrow=n_state)
         logliks <- get_backwards_likelihoods_list(phylobject, temp, Q, n_state, n_nodes, post_order_edges, topology)
         root_loglik <- logliks[[phylobject[["Nnode"]]+2]]
-        return(sum(root_loglik*par_df$prior))
+        return(sum(root_loglik + log(par_df$prior)))
     }
 
     MLE <- optim(initial_state, compute_phylobject, method="L-BFGS-B", lower=lb, control=list(fnscale=-1))
     return(list(lambda=MLE$par[1:n_state], 
                 mu=MLE$par[1:n_state+n_state], 
                 Q=matrix(MLE$par[1:n_state^2+2*n_state], nrow=n_state)))
+}
+
+logsum <- function(logw){
+  logmax = max(logw)
+  log(sum(exp(logw-logmax)))+logmax
 }
