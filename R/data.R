ef1a302aa0338f6bfee67e85c80d31f954ad7497
yexuansong <yexuan_song@sfu.ca>
2026-02-06 17:02:18 -0800

    adding descriptions for the saasi_demo.rda

--- R/data.R
+++ R/data.R
@@ -8,3 +8,27 @@
 #' data(ebola_tree)
 #' ebola_tree$root.edge
 "ebola_tree"
+
+
+
+#' Simulated tree example
+#'
+#' A simulated birth-death-sampling tree with 13 tips and 3 states used for demonstration
+#' purposes. 
+#' The dataset includes the a tree, transition rate matrix, 
+#' and birth-death-sampling parameters used for simulation.
+#'
+#' @format A list containing three objects:
+#' \describe{
+#'   \item{demo_tree_prepared}{A `phylo` tree object with tip states}
+#'   \item{demo_Q}{A 3x3 transition rate matrix}
+#'   \item{demo_pars}{A data.frame with birth-death-sampling parameters including
+#'     rootprior, lambda, mu, and psi for each state}
+#' }
+#' @source Simulated tree
+#' @examples
+#' data(saasi_demo)
+#' plot(demo_tree_prepared)
+#' demo_Q
+#' demo_pars
+"saasi_demo"
